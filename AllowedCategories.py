5367e8231b5e1e5bc92d6c3cdc5e3520ab192434
Juan <129220060+XcpBoy@users.noreply.github.com>
2026-02-28 19:32:19 -0500

    Define ALLOWED_CATEGORIES in AllowedCategories.py
    Add allowed categories for content download.

--- AllowedCategories.py
+++ AllowedCategories.py
@@ -1,1 +1,9 @@
+# AllowedCategories.py
+# Lista blanca de categorías permitidas para descargar contenido
+# Si quieres cambiar esto, tienes que comitear en GitHub ;)
 
+ALLOWED_CATEGORIES = [
+    "self improvement",
+    "strength",
+    "bodybuilding"
+]
